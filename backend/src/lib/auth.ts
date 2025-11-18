776f8e2655adb10a5d53b75a6d09827e847a03d5
code-god-jitendra <jitendra0905kumar@gmail.com>
2025-11-18 17:03:41 +0530

    fixing version incompatibility

--- backend/src/lib/auth.ts
+++ backend/src/lib/auth.ts
@@ -1,12 +1,19 @@
 import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
-import { client }  from "../config/db.js";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
 
-const db = client.db();
+dotenv.config();
+
+// Create a separate client just for Auth
+// (This is safe; MongoDB handles multiple connections efficiently)
+const client = new MongoClient(process.env.MONGODB_URI!);
+const db = client.db(); 
 
 export const auth = betterAuth({
-  database: mongodbAdapter(db, {
-    // Optional: if you don't provide a client, database transactions won't be enabled.
-    client
-  }),
+  database: mongodbAdapter(db),
+  emailAndPassword: {
+    enabled: true
+  },
+  // ... other providers
 });
