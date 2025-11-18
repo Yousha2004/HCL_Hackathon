452ce4aae4a02bccd191b5c7d2a2cef34d445c88
faizan Hussain <faizan0hussain@gmail.com>
2025-11-18 15:56:34 +0530

    added public routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,6 +4,9 @@ import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth";
 import router from "./src/routes/route";
 
+
+
+import publicRoutes from './routes/publicRoutes';
 const app = express();
 const port = 8000;
 
@@ -21,6 +24,8 @@ app.use(express.json());
 app.all("/api/auth/*", toNodeHandler(auth));
 
 app.use('/api/routes', router);
+
+app.use('/api/public', publicRoutes);
 
 
 // --- SERVER START ---
